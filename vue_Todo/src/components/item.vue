a31e258fb5642a420cf69902e5a78cbb67496742
home <1023115946@qq.com>
2018-08-10 00:28:56 +0800

    todo-1.1

--- vue_Todo/src/components/item.vue
+++ vue_Todo/src/components/item.vue
@@ -1,12 +1,25 @@
 <template>
-    
+    <div id="item">
+        <div  v-for="item in itemList" :key="item.id">
+            <p><span>{{item.content}}</span><a @click.prevent="deleteItem(item.id)">删除</a></p>
+        </div>
+    </div>
 </template>
 
 <script>
-    
+export default {
+  methods: {
+    deleteItem(id) {
+        var index = this.itemList.findIndex(element=>{
+            element.id==id
+        });
+        this.itemList.splice(index,1)
+    }
+  },
+  props: ["itemList"]
+};
 </script>
 
 
 <style lang="less" scoped>
-
 </style>
--- vue_Todo/src/components/todo.vue
+++ vue_Todo/src/components/todo.vue
@@ -1,32 +1,43 @@
 <template>
     <div id="real-app">
         <input type="text" placeholder="接下来要做什么"
-        autofocus @keyup.enter="addItem">
+        autofocus @keyup.enter="addItem" v-model="itemVal">
 
-        <item></item>
+        <item :itemList="itemList"> </item>
 
         <tabs></tabs>
     </div>
 </template>
 
 <script>
-import item from './item.vue'
-import tabs from './tabs.vue'
+import item from "./item.vue";
+import tabs from "./tabs.vue";
 
 export default {
-    data(){
-        return {
-
-        }
-    },
-    methods:{
-        addItem(){
-        }
-    },
-    components:{
-        item,
-        tabs
+  data() {
+    return {
+      id: 0, //每条item的id
+      completed: false, //item是否完成
+      itemVal: "", //文本框中的输入值
+      itemList: [] //存储所有item
+    };
+  },
+  methods: {
+    //按回车把内容添加到itemList中的第一条
+    addItem() {
+      this.id++;
+      this.itemList.unshift({
+        id: this.id,
+        content: this.itemVal.trim(),
+        completed: this.completed
+      });
+      this.itemVal=''
     }
+  },
+  components: {
+    item,
+    tabs
+  }
 };
 </script>
 
